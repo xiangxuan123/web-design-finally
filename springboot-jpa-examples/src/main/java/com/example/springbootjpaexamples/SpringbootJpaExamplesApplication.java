5ae3d9ccb81e9a41007d8d7b9aa56b0211ae58f0
xx <1141237081@qq.com>
2021-03-31 21:17:51 +0800

    First

---
+++ springboot-jpa-examples/src/main/java/com/example/springbootjpaexamples/SpringbootJpaExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootjpaexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJpaExamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJpaExamplesApplication.class, args);
+    }
+
+}
