5418ba225e9cc4d6a3756aab15232c139a3e7f48
xx <1141237081@qq.com>
2021-04-03 08:52:17 +0800

    First

---
+++ springboot-spring-examples/src/main/java/com/example/springbootspringexamples/SpringbootSpringExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootspringexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSpringExamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSpringExamplesApplication.class, args);
+    }
+
+}
---
+++ springboot-spring-examples/src/test/java/com/example/springbootspringexamples/SpringbootSpringExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootspringexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSpringExamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
